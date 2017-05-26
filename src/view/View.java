e3702ee8b644d23feb869f068c5c9fa634b41217
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2017-05-26 13:06:33 +0300

    Created a Hello World -test

---
+++ src/view/View.java
@@ -0,0 +1,13 @@
+package view;
+
+/**
+ *  Main window for the program.
+ *
+ * @author evnurm
+ */
+public class View {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
+
+}
