3fcbde6937513c975073205cd6e46d2217054e5e
Valtteri Nurminen <valtteri.e.nurminen@aalto.fi>
2017-06-06 13:47:24 +0300

    Added some management-related methods.

--- src/model/Project.java
+++ src/model/Project.java
@@ -1,23 +1,43 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 /**
  * @author evnurm
  */
 public class Project{
 
-    String title;
-    ArrayList<Task> tasks = new ArrayList<Task>();
+    private String title;
+    private Date deadline;
+    private Date beginning;
+    private ArrayList<Member> members = new ArrayList<Member>();
+    private ArrayList<Task> tasks = new ArrayList<Task>();
+
+
     public Project(String name){
         title = name;
     }
 
     /** Adds a task into this project. */
-    public void addTask(String name){
-        tasks.add(new Task(name));
+    public void addTask(Task task){
+        tasks.add(task)
     }
 
+    /** Removes the given task from this project. */
+    public removeTask(Task task){
+        tasks.remove(task);
+    }
 
+    /** Adds the given member into this project. */
+    public void addMember(Member member){ members.add(member) }
 
+    /** Removes the given member from this project. */
+    public void removeMember(Member member){ members.remove(member)}
+
+    /** Sets the deadline of the project to the given date. */
+    public void setDL(Date date){ deadline = date }
+
+    /** Sets the beginning date of the project to the given date. */
+    public void setBeginning(Date date){beginning = date}
 }
