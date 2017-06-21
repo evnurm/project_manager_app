7c18a094ab66ba31a8ba906289d80430c7505fd3
Viljami <axel.viljami.nurminen@hotmail.com>
2017-06-21 21:42:07 +0300

    Implemented an early "attempt" at signing the user in

--- src/controller/Database.java
+++ src/controller/Database.java
@@ -1,53 +1,37 @@
 package controller;
+import javax.xml.bind.DatatypeConverter;
+import javax.xml.transform.Result;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.util.Base64;
 
 /**
  * Created by Viljami on 19.6.2017.
  */
 public class Database {
 
+    Connection conn;
+    Statement statement;
     public Database(){
-        // JDBC driver name and database URL
-         final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-         final String DB_URL = "jdbc:mysql://localhost/project_manager";
+
+         final String databaseURL = "jdbc:mysql://localhost/project_manager";
 
         //  Database credentials
-         final String USER = "client";
-         final String PASS = "client";
+         final String mysqlUsername = "client";
+         final String mysqlPassword = "client";
 
 
-         Connection conn  = null;
-         Statement statement = null;
+
+
          try {
              Class.forName("com.mysql.jdbc.Driver");
 
              System.out.println("Connecting to database...");
-             conn = DriverManager.getConnection(DB_URL,USER,PASS);
+             conn = DriverManager.getConnection(databaseURL, mysqlUsername, mysqlPassword);
 
-             System.out.println("Creating statement...");
-             statement = conn.createStatement();
-             String sql;
-             sql = "SELECT * FROM Users";
-             ResultSet rs = statement.executeQuery(sql);
 
-             while(rs.next()) {
-                 //Retrieve by column name
-                 int id = rs.getInt("user_id");
-                 String user = rs.getString("username");
-                 String first = rs.getString("first_name");
-                 String last = rs.getString("last_name");
-                 String email = rs.getString("email");
-                 //Display values
-                 System.out.println("ID: " + id);
-                 System.out.println("Username: " + user);
-                 System.out.println("First name: " + first);
-                 System.out.println("Last name: " + last);
-                 System.out.println("Email: " + email);
-
-                 rs.close();
-                 statement.close();
-                 conn.close();
-             }
 
          }catch(ClassNotFoundException e){
              System.out.println("Class not found.");
@@ -56,5 +40,31 @@ public class Database {
              System.out.println("SQL Exception");
          }
 
+    }
+
+    public void signIn(String username, String passwordText){
+        try {
+            // Hashing the password using SHA-256 algorithm.
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] passwordHash = md.digest(passwordText.getBytes("UTF-8"));
+            String hashString = DatatypeConverter.printHexBinary(passwordHash).toLowerCase();
+
+            statement = conn.createStatement();
+            String sql = "SELECT user_id FROM Users WHERE username = '" +username + "' AND password  = '" +hashString +"';";
+            ResultSet rs = statement.executeQuery(sql);
+
+            while(rs.next()){
+                int id = rs.getInt("user_id");
+                System.out.println(id);
+            }
+
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
