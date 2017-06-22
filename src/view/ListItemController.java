5b7cfba74c157b3452a38cc2898015049ab04004
Alvar <alvar.wihuri@gmail.com>
2017-06-22 13:43:17 +0300

    Created empty abstract classes and some comments for list-related controllers.

---
+++ src/view/ListItemController.java
@@ -0,0 +1,10 @@
+package view;
+
+/**
+ * Created by Alvar on 22.6.2017.
+ * Abstract class for a controller of a general list item in a UI. A list item could
+ * be e.g. the ui-element of a project in the projects view or a task in the tasks view.
+ */
+public abstract class ListItemController {
+
+}
