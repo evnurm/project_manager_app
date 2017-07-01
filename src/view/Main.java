9c32d91e4c6ba560e749b28d1c5b159eb03175a8
Viljami <axel.viljami.nurminen@hotmail.com>
2017-07-01 20:45:43 +0300

    Implemented the UI for RegistrationView and th change between login and registration windows.

--- src/view/Main.java
+++ src/view/Main.java
@@ -3,6 +3,7 @@ package view;
  * Created by Alvar on 9.6.2017.
  */
 
+import controller.Database;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -12,15 +13,23 @@ import javafx.scene.Parent;
 
 public class Main extends Application {
 
+    private static  Database db;
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("projectsView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("LoginView.fxml"));
         primaryStage.setTitle("Project manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
+
+        primaryStage.setScene(new Scene(root, 400, 600));
+        db = new Database();
+
         primaryStage.show();
+    }
+
+    public static Database getDatabase(){
+        return db;
     }
 }
