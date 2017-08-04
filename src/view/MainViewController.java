47a58a6e9fe729d84d027e7bc528b8bfb31e3c2f
Alvar <alvar.wihuri@gmail.com>
2017-08-04 16:24:39 +0300

    Created a controller for the new ui and changed login to load the new ui.

---
+++ src/view/MainViewController.java
@@ -0,0 +1,19 @@
+package view;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Alvar on 4.8.2017.
+ */
+public class MainViewController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
