aa025b197daece2d6ccdf3879f15d6be287af9dd
nurmAV <axel.viljami.nurminen@hotmail.com>
2017-08-09 23:13:34 +0300

    Added a default profile picture and made it visible in the UI

--- src/view/MainViewController.java
+++ src/view/MainViewController.java
@@ -5,12 +5,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import model.Project;
 import model.User;
 
+
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -25,10 +29,12 @@ public class MainViewController implements Initializable {
     @FXML private Label usernameLabel;
     @FXML private VBox projectMenu;
     @FXML private StackPane projectInfoContainer;
+    @FXML private ImageView profilePic;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
         usernameLabel.setText(Main.getSession().getUser().getFirstName() + " " + Main.getSession().getUser().getLastName());
+        profilePic.setImage(new Image((new File("res/default.jpg")).toURI().toString()));
         projectInfoContainer.getChildren().add(new ProjectInfoView());
         try {
             ArrayList<Project> projects = Main.getDatabase().getProjects(Main.getSession().getUser().getId());
