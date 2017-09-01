c970c99bcb7012259fbd5a5fb563203032dd5fc5
nurmAV <axel.viljami.nurminen@hotmail.com>
2017-09-01 14:48:51 +0300

    Added a new window for searching users to add as members

---
+++ src/view/MemberSearch.java
@@ -0,0 +1,20 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+
+import javafx.scene.control.*;
+import java.io.IOException;
+
+
+public class MemberSearch {
+    @FXML private VBox memberResults;
+    @FXML private TextField searchBar;
+    @FXML private Button searchButton;
+    @FXML private Button addMemberButton;
+
+    public MemberSearch() {
+
+    }
+}
